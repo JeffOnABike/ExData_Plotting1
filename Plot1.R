19355b1dbbfc7d60f76edcc8072f0b3a9b9f1410
JeffOnABike <jeff.redsox@gmail.com>
2014-05-11 10:52:38 -0700

    plot1 commit

---
+++ Plot1.R
@@ -0,0 +1,16 @@
+load.data <- function() {
+        file <- "~/Documents/Coursera/Data_Science_Specialization/R_Working_Directory/household_power_consumption.txt" 
+        data <- read.table(file, sep=";", header=TRUE, col.names= c("Date", "Time", "Global_active_power", 
+                "Global_reactive_power", "Voltage", "Global_Intensity", "Sub_metering_1", "Sub_metering_2", "Sub_metering_3"), 
+                colClasses = c("character", "character", rep("numeric",7)), na.strings= "?")
+        feb1 <- data[data$Date == "2007-02-01",]
+        feb2 <- data[data$Date == "2007-02-02",]
+        feb.days <- rbind(feb1, feb2)
+}
+
+plot1 <- function(){
+        par(mfrow=c(1,1), mar= c(4,4,1,2), font=1)
+        hist(feb.days$Global_active_power, col="red", xlab = "Global Active Power (kilowatts)", ylab = "Frequency", main="Global Active Power")
+        dev.copy(png, file = "plot1.png")
+        dev.off()
+}
